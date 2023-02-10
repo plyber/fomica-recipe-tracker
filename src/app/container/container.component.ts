e36f7440eb626b6026091a082ec9483818b6e003
Oliviu <dienes.oliviu@gmail.com>
2023-02-10 13:07:40 +0200

    first commit

---
+++ src/app/container/container.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { RecipeCardComponent } from '../recipe-card/recipe-card.component';
+@Component({
+  selector: 'app-container',
+  templateUrl: './container.component.html',
+  styleUrls: ['./container.component.scss']
+})
+export class ContainerComponent {
+  
+}
