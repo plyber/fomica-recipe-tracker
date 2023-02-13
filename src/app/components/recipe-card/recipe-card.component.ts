a1a022d1c684928fc4ab85c8d4fff7c36766a260
Oliviu <dienes.oliviu@gmail.com>
2023-02-13 23:31:57 +0200

    add styling

--- src/app/components/recipe-card/recipe-card.component.ts
+++ src/app/components/recipe-card/recipe-card.component.ts
@@ -1,14 +1,15 @@
-
-import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { CardModel } from '../../models/card.model'
 
 @Component({
   selector: 'app-recipe-card',
   templateUrl: './recipe-card.component.html',
-  styleUrls: ['./recipe-card.component.scss']
+  styleUrls: [ './recipe-card.component.scss' ]
 })
 
 export class RecipeCardComponent {
+
   @Input() card!: CardModel;
+
 }
 
